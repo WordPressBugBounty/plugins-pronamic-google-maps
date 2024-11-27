3161d5d44fbe76eb0ad6534ed44a9f2f136db16c
Sean Murphy <sean@iamseanmurphy.com>
2024-11-27 14:17:21 +0000

    2.3.3

---
+++ pronamic-google-maps/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pronamic/wp-pronamic-google-maps',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b0934b8b5a052cad29bc084a7d45655276f49730',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pronamic/wp-pronamic-google-maps' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b0934b8b5a052cad29bc084a7d45655276f49730',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
